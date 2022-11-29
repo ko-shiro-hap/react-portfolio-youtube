9a36fbd6535e585e105ea58c532611de6bf97054
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-11-29 23:46:43 +0900

    メインセクション作成

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,34 @@ function App() {
           </nav>
         </div>
       </header>
+
+      <section className="text-gray-700" id="home">
+        <div className="flex container mx-auto py-20 px-5 flex-col md:flex-row items-center">
+          <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
+            <h1 className="text-3xl sm:text-6xl text-gray-900 font-medium mb-4">
+              Hi!
+              <br />
+              I'm KouCode
+              <br />
+              Web Developer
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
+              repudiandae possimus, minus provident totam neque delectus quasi
+              cum unde quia!
+            </p>
+            <button className="text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300">
+              Contact
+            </button>
+          </div>
+          <div className="md:w-1/2 lg:max-w-lg w-5/6">
+            <img
+              src="./images/shivendu-shukla-ghcT2OfTHqc-unsplash.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
